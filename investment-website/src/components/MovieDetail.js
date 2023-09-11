03f50ab050e125f45a1a12c9c4ade7cba69009a8
Josh <lutz_143@yahoo.com>
2023-09-10 20:28:43 -0400

    updates to src components to render API to front end

---
+++ investment-website/src/components/MovieDetail.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function MovieDetail(props) {
+  return (
+    <div className="text-center">
+      <img
+        alt={props.ticker}
+        className="img-fluid"
+        src={props.src}
+        style={{ margin: '0 auto' }}
+      />
+      <h3>Director(s): {props.ticker}</h3>
+      {/* <h3>Genre: {props.genre}</h3>
+      <h3>Released: {props.released}</h3> */}
+    </div>
+  );
+}
+
+export default MovieDetail;
