c8a332cdea718e61fa8b0abcc2ae2f725abf4db6
Josh <lutz_143@yahoo.com>
2023-09-30 07:32:31 -0400

    added user controller and d3 dependency

--- investment-website/server/models/User.js
+++ investment-website/server/models/User.js
@@ -35,6 +35,13 @@ User.init(
         len: [8],
       },
     },
+    valuation_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'valuation',
+        key: 'id'
+      }
+    },
   },
   {
     hooks: {
