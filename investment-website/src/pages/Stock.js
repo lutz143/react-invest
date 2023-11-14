43930d12623804484eee807b00bcd6e934e742b5
Josh <lutz_143@yahoo.com>
2023-11-13 21:07:03 -0500

    updates to Stock page for additional data attributes, styling, and tooltip feature

--- investment-website/src/pages/Stock.js
+++ investment-website/src/pages/Stock.js
@@ -1,14 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import { Button } from 'react-bootstrap';
+import { Button, Card, Container, Row, Col } from 'react-bootstrap';
+import { Tooltip as ReactTooltip } from "react-tooltip";
 import { useSelector } from "react-redux";
 import axios from 'axios';
-
+import Papa from 'papaparse';
+import moment from 'moment';
 // import * as d3 from 'd3';
 // import axios from 'axios';
 
+import classes from "./Stock.module.css";
+
 import PageContainer from "../containers/PageContainer";
-import classes from "./Game.module.css";
 
 const Stock = () => {
   const user = useSelector((state) => state.auth.user);
@@ -23,8 +26,26 @@ const Stock = () => {
     // Make a GET request to API endpoint by stock ID
     axios.get(`http://localhost:3001/api/valuations/${id}`)
       .then(response => {
-        setValuation(response.data);
-        console.log(setValuation);
+        const stock = response.data
+        const formattedData = {
+          ...stock,
+          Assessment_Date: moment(stock.Assessment_Date).format('M/DD/YYYY'),
+          previousClose: parseFloat(stock.previousClose).toFixed(2),
+          previousCloseFloat: stock.previousClose,
+          CAGR_CPS: parseFloat(stock.CAGR_CPS).toFixed(2),
+          NOM_CPS: parseFloat(stock.NOM_CPS).toFixed(2),
+          CON_CPS: parseFloat(stock.CON_CPS).toFixed(2),
+          CONF_NOM: parseFloat(stock.CONF_NOM).toFixed(0),
+          toolTip_CONF_CAGR: "CAGR Confidence: " + parseFloat(stock.CONF_CAGR).toFixed(0),
+          marketCap: (stock.marketCap).toLocaleString('en-US'),
+          sharesOutstanding: (stock.sharesOutstanding).toLocaleString('en-US'),
+          Terminal_Rate: (stock.Terminal_Rate * 100).toFixed(1) + "%",
+          WACC: (stock.WACC * 100).toFixed(1) + "%",
+          toolTip_Swing_NOM: "NOM-CON Swing: " + (stock.Swing_NOM * 100).toFixed(1) + "%",
+          toolTip_TerminalValue_NOM: "NOM Terminal Value: " + (stock.TerminalValue_NOM).toLocaleString('en-US'),
+          toolTip_NPV_Total_NOM: "NOM NPV: " + (stock.NPV_Total_NOM).toLocaleString('en-US'),
+        }
+        setValuation(formattedData);
       })
       .catch(error => {
         console.error('Error fetching data:', error);
@@ -38,6 +59,26 @@ const Stock = () => {
   }, [id]); // include "id" in the dependency array
 
   // const svgRef = useRef();
+
+  const downloadCsv = () => {
+    // convert stock data to CSV format
+    const stockArray = [stock];
+    const csv = Papa.unparse(stockArray);
+    
+    // trigger download option
+    downloadFile(csv, 'data.csv');
+  }
+
+  const downloadFile = (content, filename) => {
+    const blob = new Blob([content], { type: 'text/csv' });
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = filename;
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+  };
 
   const newPortfolioStock = async (event) => {
 
@@ -66,28 +107,102 @@ const Stock = () => {
 
   return (
     <PageContainer title="Stock Details">
-        <div>
+      <section>
+        <Container>
+          <Row lg={1}>
+            <Card className='mb-3'>
+              <Card.Header>
+                <Row>
+                  <h1 className='d-flex justify-content-between'>{stock.Ticker}<Button onClick={downloadCsv}>&#11123;</Button></h1>                    
+                </Row>
+              </Card.Header>
+              <Card.Body>
+                <Container>
+                  <Row>
+                    <Col>Previous Close: {stock.previousClose}</Col>
+                    <Col>
+                      <span data-tooltip-id="cagr-conf-tooltip">                        
+                        CAGR Estimate: {stock.CAGR_CPS}
+                      </span>
+                    </Col>
+                  </Row>
+                  <Row>
+                    <Col>
+                      <span data-tooltip-id="nom-con-tooltip">
+                        Nominal Estimate: {stock.NOM_CPS}
+                      </span>
+                    </Col>
+                    <Col>
+                      <span data-tooltip-id="nom-con-tooltip">
+                        Conservative Estimate: {stock.CON_CPS}
+                      </span>
+                    </Col>
+                  </Row>
+                  <Row className='mb-2'>
+                    <Col>
+                      <span>
+                        Confidence: {stock.CONF_NOM}
+                      </span>
+                    </Col>
+                  </Row>
+                </Container>
+                <Row className={classes.cardDivider}></Row>
+
+                <Container className={classes.cardSubSection}>
+                  <Row>
+                    <Col>Market Cap: {stock.marketCap}</Col>
+                    <Col>Shares Outstanding: {stock.sharesOutstanding}</Col>
+                  </Row>
+                  <Row className='mb-2'>
+                    <Col>
+                      <span data-tooltip-id="terminal-value-tooltip">
+                        Terminal Rate: {stock.Terminal_Rate}
+                      </span>
+                    </Col>
+                    <Col>
+                      <span data-tooltip-id="nom-npv-tooltip">
+                        WACC: {stock.WACC}
+                      </span>
+                    </Col>
+                  </Row>
+                </Container>
+
+              </Card.Body>
+            </Card>
+          </Row>
+
           {/* <div>
             <svg ref={svgRef}></svg>
           </div> */}
-          <div className={classes.detailHolder}>
-            <h1 className={classes.stockTitle}>{stock.Ticker}</h1>
-            <p>{user}, id = {user_id}</p>
-            <ul>
-              {portfolioIds.map(id => (
-                <li key={id}>{id}</li>
-              ))}
-            </ul>
-            <p>{stock.previousClose}</p>
-            <p>{stock.id}</p>
-
+            
             {error ? <p>{error}</p>: null}
             {user ? <div>
               {added.includes(stock.id) || portfolioIds.includes(stock.id) ? <div>Added!</div> : <Button onClick={() => newPortfolioStock()}>Add Stock</Button>}
               </div> : null}
 
-          </div>
-        </div>
+        </Container>
+
+          <ReactTooltip
+            id="nom-con-tooltip"
+            place="bottom"
+            content= {stock.toolTip_Swing_NOM}
+          />
+          <ReactTooltip
+            id="terminal-value-tooltip"
+            place="bottom"
+            content= {stock.toolTip_TerminalValue_NOM}
+          />
+          <ReactTooltip
+            id="nom-npv-tooltip"
+            place="bottom"
+            content= {stock.toolTip_NPV_Total_NOM}
+          />
+          <ReactTooltip
+            id="cagr-conf-tooltip"
+            place="bottom"
+            content= {stock.toolTip_CONF_CAGR}
+          />
+      </section>
     </PageContainer>
   );
 }
