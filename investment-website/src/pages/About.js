50157d627bb5e5866108c611038bae1a478f28c7
Josh <lutz_143@yahoo.com>
2024-02-26 12:32:07 -0500

    updates to About page content

--- investment-website/src/pages/About.js
+++ investment-website/src/pages/About.js
@@ -1,5 +1,5 @@
-import { NavLink } from "react-router-dom";
 import ProfilePic from '../images/JoshLutz289635.jpg'
+import { Link } from 'react-router-dom';
 
 import "./About.css";
 
@@ -12,20 +12,16 @@ function About () {
       <section className="our-purpose-container">
         <h1 className="about-header">Our Purpose</h1>
         <p className="leading">
-          Every day we open our doors and welcome millions of hungry people. 
-          But they’re not just hungry for food. 
-          They’re hungry for something that can make their lives healthier, easier, brighter and a bit lighter. 
-          They’re hungry to feel good, do well, to go from anxiety to inspiration.
+          At <span style={{fontFamily: "Gloria Hallelujah, cursive"}}>nominal</span>, we are dedicated to connecting a community of 
+          knowledge to advance your personal wealth, so you can spend more time with the things that matter.
         </p>
         <p>
-          So whether we manufacture, market, stock, check, deliver, or manage, we all try to make their experience as uplifting as possible. 
-          Just like Barney Kroger did when he brought together the butcher, the baker, dry goods, and produce all under one roof. 
-          We’ve always been about value, convenience, and making customers’ lives simpler.
+          Whether you are progressing your career or busy developing a family, nominal connects like-minded investors to empower them 
+          in advancing their financial goals.  The nominal community aims to provide scalable financial data modeling, ideas, and 
+          support to enable research and analysis, putting the power of sound investment in your hands.
         </p>
         <p>
-          So whether we manufacture, market, stock, check, deliver, or manage, we all try to make their experience as uplifting as possible. 
-          Just like Barney Kroger did when he brought together the butcher, the baker, dry goods, and produce all under one roof. 
-          We’ve always been about value, convenience, and making customers’ lives simpler.
+          <Link to="/login" className='about-link'>Connect</Link> today and elevate your portfolio to nominal!
         </p>
         
         <h1 className="about-header">The Team</h1>
@@ -37,14 +33,11 @@ function About () {
           </div>
           <div className="team-description">
             <p>
-              So whether we manufacture, market, stock, check, deliver, or manage, we all try to make their experience as uplifting as possible. 
-              Just like Barney Kroger did when he brought together the butcher, the baker, dry goods, and produce all under one roof. 
-              We’ve always been about value, convenience, and making customers’ lives simpler.
-            </p>
-            <p>
-              So whether we manufacture, market, stock, check, deliver, or manage, we all try to make their experience as uplifting as possible. 
-              Just like Barney Kroger did when he brought together the butcher, the baker, dry goods, and produce all under one roof. 
-              We’ve always been about value, convenience, and making customers’ lives simpler.
+              Josh Lutz is the founder and architect of nominal.com.  He currently is a manager at a 
+              leading defense firm where he utilizes his knowledge in data management, business analytics, 
+              and machine learning to drive sound financial decisions.  His experience expands upon his 
+              interest in providing a forum where community can enable investment ideas, leading to a nominal 
+              approach in investing in a successful tomorrow.
             </p>
           </div>
         </section>
